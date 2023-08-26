e8c632e8bd3096caefb5022b21ec8ea6b14e82fd
roshan.waa <kumroshan120@gmail.com>
2023-08-26 18:06:20 +0530

    Slice Off Array and string

---
+++ 14.0 Introduction to Javascript ES6/SliceMethod.js
@@ -0,0 +1,16 @@
+let name = 'Roshan';
+const newLocal = name.slice(1, 5);
+
+var tweet =
+  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi aut recusandae provident beatae, totam dignissimos numquam, quae labore eos distinctio obcaecati iste nesciunt quidem rem, at id ullam tempora debitis.';
+
+var tweetUnder140 = tweet.slice(0, 140);
+
+console.log(tweet.length);
+console.log(tweetUnder140);
+
+var arr = [1, 2, 1, 3, 4, 5, 6, 7, 8, 9, 10, 14, 125, 1, 4, 21];
+arr.slice(3, 10);
+console.log(arr.slice(3, 10));
+
+console.log(arr.length);
