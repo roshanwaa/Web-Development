080993d4473b694c08bde4d279fe1960e3aafbde
roshan.waa <kumroshan120@gmail.com>
2023-08-26 18:13:29 +0530

    Upercase And lower case Method

---
+++ 14.0 Introduction to Javascript ES6/toUperCaseMethod.js
@@ -0,0 +1,9 @@
+let str =
+  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed asperiores assumenda eveniet possimus ullam temporibus reiciendis voluptas, sapiente esse vel perspiciatis totam magnam aspernatur commodi, consequuntur, nemo molestias obcaecati dolor.';
+
+console.log(str.toUpperCase() + ' \n');
+
+str =
+  'LOREM IPSUM DOLOR SIT AMET CONSECTETUR ADIPISICING ELIT. SED ASPERIORES ASSUMENDA EVENIET POSSIMUS ULLAM TEMPORIBUS REICIENDIS VOLUPTAS, SAPIENTE ESSE VEL PERSPICIATIS TOTAM MAGNAM ASPERNATUR COMMODI, CONSEQUUNTUR, NEMO MOLESTIAS OBCAECATI DOLOR.';
+
+console.log(str.toLowerCase());
