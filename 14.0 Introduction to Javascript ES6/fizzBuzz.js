5c70adbe777d2afe8afe0599026168f8a3b1d427
roshan.waa <kumroshan120@gmail.com>
2023-08-28 17:24:53 +0530

    add Fizz fun using for loop method

--- 14.0 Introduction to Javascript ES6/fizzBuzz.js
+++ 14.0 Introduction to Javascript ES6/fizzBuzz.js
@@ -17,3 +17,38 @@ function fizzBuzz() {
 }
 
 fizzBuzz();
+
+function fizzBuzzLoopMethod(n) {
+  // Declare an array to store the results
+  let result = [];
+
+  for (let i = 0; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      // Check if i is divisible by both 3 and 5
+      // Add "FizzBuzz" to the result array
+      result.push('FizzBuzz');
+    } else if (i % 3 === 0) {
+      // Check if i is divisible by 3
+      // Add "Fizz" to the result array
+      result.push('Fizz');
+    } else if (i % 5 === 0) {
+      // Check if i is divisible by 5
+      // Add "Buzz" to the result array
+      result.push('Buzz');
+    } else {
+      // Add the current number as a string to the
+      // result array
+      result.push(i.toString());
+    }
+  }
+  return result;
+}
+
+// Driver code
+let n = 20;
+
+// Call the fizzBuzz function to get the result
+let result = fizzBuzzLoopMethod(n);
+
+// Print the result
+console.log(result.join(' '));
