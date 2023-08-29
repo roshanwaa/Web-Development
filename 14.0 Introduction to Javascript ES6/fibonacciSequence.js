5a08ffe2c27d7cab15e8c079378678d2f0303649
roshan.waa <kumroshan120@gmail.com>
2023-08-29 17:34:36 +0530

    fibonacciGenerator Solution

--- 14.0 Introduction to Javascript ES6/fibonacciSequence.js
+++ 14.0 Introduction to Javascript ES6/fibonacciSequence.js
@@ -15,3 +15,24 @@ for (let i = 1; i <= number; i++) {
   n1 = n2;
   n2 = nextTerm;
 }
+
+function fibonacciGenerator(n) {
+  //Do NOT change any of the code above 👆
+  //Write your code here:
+  var output = [];
+  if (n === 1) {
+    output = [0];
+  } else if (n === 2) {
+    output = [0, 1];
+  } else {
+    output = [0, 1];
+    for (var i = 2; i < n; i++) {
+      output.push(output[output.length - 2] + output[output.length - 1]);
+    }
+  } //Return an array of fibonacci numbers starting from 0.
+  return output;
+  //Do NOT change any of the code below 👇
+}
+
+output = fibonacciGenerator(12);
+console.log(output);
