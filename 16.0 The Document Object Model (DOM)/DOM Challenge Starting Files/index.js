13be78b3c2a8f0711b9fe3449c9dce0241bd7412
roshan.waa <kumroshan120@gmail.com>
2023-09-03 21:00:19 +0530

    get href attribute from a tag

--- 16.0 The Document Object Model (DOM)/DOM Challenge Starting Files/index.js
+++ 16.0 The Document Object Model (DOM)/DOM Challenge Starting Files/index.js
@@ -9,11 +9,16 @@ changedColorBtn.style.background = 'yellow';
 
 document.querySelector('#title').classList.add('huge');
 
-const HelloText = (document.querySelector('#title').innerText = ' Say Hello '); // its also change the title main content of the document
-console.log(HelloText);
+// const HelloText = (document.querySelector('#title').innerText = ' Say Hello '); // its also change the title main content of the document
+// console.log(HelloText);
+
 const text = (document.querySelector('#title').innerHTML =
   '<em>Roshan.Waa</em>'); // its also change the title main content of the document
-console.log(HelloText);
+console.log(text);
+
+// To get the a tag href attribute
+const getAttribute = document.querySelector('a').getAttribute('href');
+console.log(getAttribute);
 
 // const gdByeText = (document.querySelector('#title').textContent =
 //   '<em>Good Bye</em>'); // its also change the title main content  of the document
