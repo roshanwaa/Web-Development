bea2280474679e62a8ab28d0f547680096332218
roshan.waa <kumroshan120@gmail.com>
2023-09-05 17:04:36 +0530

    Debug the function

--- 18.0 Advanced Javascript and DOM Manipulation/Drum Kit Starting Files/main.js
+++ 18.0 Advanced Javascript and DOM Manipulation/Drum Kit Starting Files/main.js
@@ -1,4 +1,4 @@
-// Higher order functions : higher order functionsare functions that can take other function a output
+// Higher order functions : higher order functionsare functions that can take other function a output || Higher order functions are functions that take one or more functions as arguments, or return a function as their result.
 
 function add(num1, num2) {
   return num1 + num2;
@@ -7,9 +7,18 @@ function add(num1, num2) {
 function multiply(n1, n2) {
   return n1 * n2;
 }
-function calculate(num1, num2, operators) {
-  return operators(num1, num2);
+
+function subtract(num1, num2) {
+  return num1 - num2;
 }
 
-debugger;
-calculate(3, 4, multiply);
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+function calculator(num1, num2, operator) {
+  return operator(num1, num2);
+}
+
+const result = calculator(3, 4, divide);
+console.log(result);
