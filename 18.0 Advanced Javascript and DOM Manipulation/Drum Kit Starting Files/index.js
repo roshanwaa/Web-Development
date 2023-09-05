a1b35ff28d5ce1b1909995db212de9f7a20b249a
roshan.waa <kumroshan120@gmail.com>
2023-09-05 23:49:52 +0530

    use This Keyword for show current dom

--- 18.0 Advanced Javascript and DOM Manipulation/Drum Kit Starting Files/index.js
+++ 18.0 Advanced Javascript and DOM Manipulation/Drum Kit Starting Files/index.js
@@ -2,7 +2,9 @@ const drumLength = document.querySelectorAll('.drum').length;
 console.log(drumLength);
 
 for (let i = 0; i < drumLength; i++) {
-  document.querySelectorAll('.drum')[i].addEventListener('click', () => {
-    console.log(i);
+  document.querySelectorAll('.drum')[i].addEventListener('click', function () {
+    console.log(this);
+    // let audio = new Audio('./sounds/crash.mp3');
+    // audio.play();
   });
 }
