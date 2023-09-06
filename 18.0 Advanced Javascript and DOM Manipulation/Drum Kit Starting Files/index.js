12113e5201f886ac1d794bf653d01a6ed57fa76f
roshan.waa <kumroshan120@gmail.com>
2023-09-06 15:34:15 +0530

    add Switch case statement to page

--- 18.0 Advanced Javascript and DOM Manipulation/Drum Kit Starting Files/index.js
+++ 18.0 Advanced Javascript and DOM Manipulation/Drum Kit Starting Files/index.js
@@ -3,8 +3,39 @@ console.log(drumLength);
 
 for (let i = 0; i < drumLength; i++) {
   document.querySelectorAll('.drum')[i].addEventListener('click', function () {
-    console.log(this);
-    // let audio = new Audio('./sounds/crash.mp3');
-    // audio.play();
+    var btnInnerHtml = this.innerHTML;
+
+    switch (btnInnerHtml) {
+      case 'w':
+        var w = new Audio('./sounds/tom-1.mp3');
+        w.play();
+        break;
+      case 'a':
+        var a = new Audio('./sounds/tom-2.mp3');
+        a.play();
+        break;
+      case 's':
+        var s = new Audio('./sounds/tom-3.mp3');
+        s.play();
+        break;
+      case 'd':
+        var d = new Audio('./sounds/tom-4.mp3');
+        d.play();
+        break;
+      case 'j':
+        var j = new Audio('./sounds/snare.mp3');
+        j.play();
+        break;
+      case 'k':
+        var k = new Audio('./sounds/crash.mp3');
+        k.play();
+        break;
+      case 'l':
+        var l = new Audio('./sounds/kick-bass.mp3');
+        l.play();
+        break;
+      default:
+        break;
+    }
   });
 }
