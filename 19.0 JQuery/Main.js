964d961a71bc5696964c03ae83313b028d2771ba
roshan.waa <kumroshan120@gmail.com>
2023-09-09 15:12:06 +0530

    Add

--- 19.0 JQuery/Main.js
+++ 19.0 JQuery/Main.js
@@ -5,3 +5,10 @@ $(document).ready(function () {
 // Selecting Elements
 $('h1');
 console.log($('h1'));
+console.log($('button'));
+
+// Get the currently value of selected element
+const currentColorValue = $('h1').css('color');
+const currentFontValue = $('h1').css('font-size');
+console.log(currentFontValue);
+console.log(currentColorValue);
