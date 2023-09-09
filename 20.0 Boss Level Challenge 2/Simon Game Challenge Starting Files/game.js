34b13a9df99b3c506abc62b0c59c17fc5c2fdb6c
roshan.waa <kumroshan120@gmail.com>
2023-09-10 01:24:22 +0530

    . Step 3 show Sequence to User with Animations and Sounds

--- 20.0 Boss Level Challenge 2/Simon Game Challenge Starting Files/game.js
+++ 20.0 Boss Level Challenge 2/Simon Game Challenge Starting Files/game.js
@@ -1,4 +1,4 @@
-//3. At the top of the game.js file, create a new array called buttonColours and set it to hold the sequence "red", "blue", "green", "yellow" .
+//3. At the top of the game.js file, create a new array called buttonColors and set it to hold the sequence "red", "blue", "green", "yellow" .
 let buttonColors = ['red', 'blue', 'green', 'yellow'];
 
 //5. At the top of the game.js file, create a new empty array called gamePattern.
@@ -11,13 +11,22 @@ function nextSequence() {
   //     buttonColors[Math.floor(Math.random() * buttonColors.length)];
   let randomNumber = Math.floor(Math.random() * 4);
 
-  //4. Create a new variable called randomChosenColour and use the randomNumber from step 2 to select a random colour from the buttonColours array.
+  //4. Create a new variable called randomChosenColor and use the randomNumber from step 2 to select a random color from the buttonColors array.
   let randomChosenColor = buttonColors[randomNumber];
 
-  //6. Add the new randomChosenColour generated in step 4 to the end of the gamePattern.
+  //6. Add the new randomChosenColor generated in step 4 to the end of the gamePattern.
   return gamePattern.push(randomChosenColor);
 }
 
 nextSequence();
 // console.log(nextSequence());
 console.log(gamePattern);
+
+$('document').ready(function () {
+  $(`#${gamePattern}`).on('click', function () {
+    $(`#${gamePattern}`).fadeOut(100);
+    $(`#${gamePattern}`).fadeIn(100);
+    const audioElement = new Audio(`./sounds/${gamePattern}.mp3`);
+    audioElement.play();
+  });
+});
