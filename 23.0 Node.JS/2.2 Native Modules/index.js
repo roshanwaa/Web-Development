b9563c325e95ffd5e9cae10957bce8e14ac17a7a
Roshan Kumar <67013997+roshanwaa@users.noreply.github.com>
2023-09-15 23:54:36 +0530

    use readFile to message

--- 23.0 Node.JS/2.2 Native Modules/index.js
+++ 23.0 Node.JS/2.2 Native Modules/index.js
@@ -1,8 +1,16 @@
 const fs = require('fs');
 
-fs.writeFile('message.txt', 'Hello from node.js', (err, data) => {
+// fs.writeFile('message.txt', 'Hello from node.js', (err, data) => {
+//   if (err) {
+//     console.error(err);
+//   }
+//   console.log('File Saved ');
+// });
+
+fs.readFile('./message.txt', 'utf8', (err, data) => {
   if (err) {
     console.error(err);
+    return;
   }
-  console.log('File Saved ');
+  console.log(data);
 });
