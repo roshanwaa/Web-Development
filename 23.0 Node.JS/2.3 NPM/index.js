026f6aefb67a4bf16ae5cdf175b71266398e6793
Roshan Kumar <67013997+roshanwaa@users.noreply.github.com>
2023-09-16 20:15:03 +0530

    The NPM Package Manager and Installing External Node Modules

--- 23.0 Node.JS/2.3 NPM/index.js
+++ 23.0 Node.JS/2.3 NPM/index.js
@@ -1,3 +1,12 @@
-var generateName = require('sillyname');
+import generateName from 'sillyname';
+import { all, random } from 'superheroes';
+
 var sillyName = generateName();
 console.log(sillyName);
+
+all;
+//=> ['3-D Man', 'A-Bomb', …]
+
+let names = random();
+//=> 'Spider-Ham'
+console.log("I'am " + names);
