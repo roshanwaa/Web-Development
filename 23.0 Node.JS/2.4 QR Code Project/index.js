f85c875f0bb6ed5526e8f5d0760acb3d29afd19d
Roshan Kumar <67013997+roshanwaa@users.noreply.github.com>
2023-09-16 22:54:58 +0530

    add qr code function

--- 23.0 Node.JS/2.4 QR Code Project/index.js
+++ 23.0 Node.JS/2.4 QR Code Project/index.js
@@ -3,3 +3,20 @@
 2. Use the qr-image npm package to turn the user entered URL into a QR code image.
 3. Create a txt file to save the user input using the native fs node module.
 */
+
+import fs from 'fs';
+import qr from 'qr-image';
+
+let url = fs.readFile('./URL.txt', 'utf8', (err, data) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(data);
+});
+
+var qr_svg = qr.image(url, { type: 'svg' });
+qr_svg.pipe(require('fs').createWriteStream('i_love_qr.svg'));
+
+var svg_string = qr.imageSync(url, { type: 'svg' });
+console.log(svg_string);
