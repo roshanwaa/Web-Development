e9cd9ada0d095367bdd4567bf533f194087b5161
Roshan Kumar <67013997+roshanwaa@users.noreply.github.com>
2023-09-17 17:18:52 +0530

    manage all files

--- 23.0 Node.JS/2.1 Using Node/index.js
+++
@@ -1,3 +0,0 @@
-// Node REPL => Read Eval Print Loop (is a computer environment where user inputs are read and evaluated, and then the results are returned to the user.)
-
-console.log('Hello From Node REPL');
