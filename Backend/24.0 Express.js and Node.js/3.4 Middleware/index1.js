08cb740e02445e3adf940f526bde019e369205c7
Roshan Kumar <67013997+roshanwaa@users.noreply.github.com>
2023-09-21 18:35:18 +0530

    install nodemon

--- Backend/24.0 Express.js and Node.js/3.4 Middleware/index1.js
+++ Backend/24.0 Express.js and Node.js/3.4 Middleware/index1.js
@@ -1,13 +1,13 @@
-import express from "express";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+import express from 'express';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 app.listen(port, () => {
