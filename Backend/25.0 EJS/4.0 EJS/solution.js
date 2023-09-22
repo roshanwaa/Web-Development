f406289f1bd26de3c6928e33cf744db564985a91
Roshan Kumar <67013997+roshanwaa@users.noreply.github.com>
2023-09-23 01:07:15 +0530

    Format the Solution

--- Backend/25.0 EJS/4.0 EJS/solution.js
+++ Backend/25.0 EJS/4.0 EJS/solution.js
@@ -1,9 +1,9 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   const today = new Date();
   //Test code
   // weekend:
@@ -13,15 +13,15 @@ app.get("/", (req, res) => {
   const day = today.getDay();
 
   // console.log(day);
-  let type = "a weekday";
+  let type = 'a weekday';
   let adv = "it's time to work hard";
 
   if (day === 0 || day === 6) {
-    type = "the weekend";
+    type = 'the weekend';
     adv = "it's time to have some fun";
   }
 
-  res.render("solution.ejs", {
+  res.render('solution.ejs', {
     dayType: type,
     advice: adv,
   });
