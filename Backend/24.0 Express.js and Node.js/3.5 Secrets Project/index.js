5e0912fdc858a9fb1b74a726443252eb909e2f04
Roshan Kumar <67013997+roshanwaa@users.noreply.github.com>
2023-09-23 00:46:49 +0530

    Secrets Access Project self

--- Backend/24.0 Express.js and Node.js/3.5 Secrets Project/index.js
+++ Backend/24.0 Express.js and Node.js/3.5 Secrets Project/index.js
@@ -1,1 +1,33 @@
 //The password is ILoveProgramming
+/* =================================================================== */
+
+import bodyParser from 'body-parser';
+import express from 'express';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
+var password = '';
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  console.log(req.body);
+  password = req.body['password'];
+  next();
+});
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.post('/check', (req, res) => {
+  if (password === 'ILoveProgramming') {
+    res.sendFile(__dirname + '/public/secret.html');
+  } else {
+    res.sendFile(__dirname + '/public/index.html');
+  }
+});
+
+app.listen(3000);
