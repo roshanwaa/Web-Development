bec83fa32248c980281379ce1adbf58fe6841b28
Roshan Kumar <67013997+roshanwaa@users.noreply.github.com>
2023-09-23 00:51:10 +0530

    Check solution Secrets Access Project

--- Backend/24.0 Express.js and Node.js/3.5 Secrets Project/index.js
+++ Backend/24.0 Express.js and Node.js/3.5 Secrets Project/index.js
@@ -8,25 +8,43 @@ import { fileURLToPath } from 'url';
 
 const app = express();
 const __dirname = dirname(fileURLToPath(import.meta.url));
-var password = '';
+// var password = '';
+var userIsAuthorized = false;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-  console.log(req.body);
-  password = req.body['password'];
+function passwordCheck(req, res, next) {
+  const password = req.body['password'];
+  if (password === 'ILoveProgramming') {
+    userIsAuthorized = true;
+  }
   next();
-});
+}
 
-app.get('/', function (req, res) {
+// app.use((req, res, next) => {
+//   console.log(req.body);
+//   password = req.body['password'];
+//   next();
+// });
+
+app.use(passwordCheck);
+
+app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
 app.post('/check', (req, res) => {
-  if (password === 'ILoveProgramming') {
+  //   if (password === 'ILoveProgramming') {
+  //     res.sendFile(__dirname + '/public/secret.html');
+  //   } else {
+  //     res.sendFile(__dirname + '/public/index.html');
+  //   }
+
+  if (userIsAuthorized) {
     res.sendFile(__dirname + '/public/secret.html');
   } else {
     res.sendFile(__dirname + '/public/index.html');
+    //Alternatively res.redirect("/");
   }
 });
 
