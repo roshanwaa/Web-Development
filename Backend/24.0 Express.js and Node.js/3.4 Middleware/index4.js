d99fe93cf6d44a8537db42f6fd2b41fe48937d4b
Roshan Kumar <67013997+roshanwaa@users.noreply.github.com>
2023-09-22 22:16:38 +0530

    solve band name by self

--- Backend/24.0 Express.js and Node.js/3.4 Middleware/index4.js
+++ Backend/24.0 Express.js and Node.js/3.4 Middleware/index4.js
@@ -1,7 +1,30 @@
-import express from "express";
+import bodyParser from 'body-parser';
+import express from 'express';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const port = 3000;
 
 const app = express();
-const port = 3000;
+var bandName = '';
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  console.log(req.body);
+  bandName = req.body['street'] + req.body['pet'];
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.post('/submit', (req, res) => {
+  req.body;
+  console.log(res.send(`<h1>bandName</h1>`));
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
