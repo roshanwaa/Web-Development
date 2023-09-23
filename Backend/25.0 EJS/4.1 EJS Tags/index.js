61bd813be8229a1a3e613b175311e2583e583a0d
Roshan Kumar <67013997+roshanwaa@users.noreply.github.com>
2023-09-23 22:22:31 +0530

    add ignore module and formate file

--- Backend/25.0 EJS/4.1 EJS Tags/index.js
+++ Backend/25.0 EJS/4.1 EJS Tags/index.js
@@ -1,15 +1,15 @@
-import express from "express";
+import express from 'express';
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   const data = {
-    title: "EJS Tags",
+    title: 'EJS Tags',
     seconds: new Date().getSeconds(),
-    items: ["apple", "banana", "cherry"],
-    htmlContent: "<strong>This is some strong text</strong>",
+    items: ['apple', 'banana', 'cherry'],
+    htmlContent: '<strong>This is some strong text</strong>',
   };
-  res.render("index.ejs", data);
+  res.render('index.ejs', data);
 });
 
 app.listen(port, () => {
