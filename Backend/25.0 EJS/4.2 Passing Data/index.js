f264dd5bda488b0929454de41e843c1acda02794
Roshan Kumar <67013997+roshanwaa@users.noreply.github.com>
2023-09-24 22:27:01 +0530

    solve first name and last name

--- Backend/25.0 EJS/4.2 Passing Data/index.js
+++ Backend/25.0 EJS/4.2 Passing Data/index.js
@@ -1,14 +1,30 @@
-import express from "express";
-import bodyParser from "body-parser";
+import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {});
+let fName = '';
+let lName = '';
 
-app.post("/submit", (req, res) => {});
+app.use((req, res, next) => {
+  fName = req.body['fName'];
+  lName = req.body['lName'];
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.render('index.ejs');
+  // res.json('ok');
+});
+
+app.post('/submit', (req, res) => {
+  // res.json({ fName: fName.length, lName: lName.length });
+  let nameLength = fName.length + lName.length;
+  res.render('index.ejs', { nameLength });
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
