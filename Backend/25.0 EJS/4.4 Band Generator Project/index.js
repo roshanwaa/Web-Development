9c835c4acc27d1cfa6c01d2b01ab5bad8914e6c9
Roshan Kumar <67013997+roshanwaa@users.noreply.github.com>
2023-09-26 19:42:09 +0530

    4.4 EJS Band Gen Solve

--- Backend/25.0 EJS/4.4 Band Generator Project/index.js
+++ Backend/25.0 EJS/4.4 Band Generator Project/index.js
@@ -4,13 +4,14 @@ import bodyParser from 'body-parser';
 const app = express();
 const port = 3000;
 
+//Step 4 - Add a dynamic year to the footer.
+//Hint: Google to find out how to get the current year using JS.
+
 //Step 3 - Make the styling show up.
 //Hint 1: CSS files are static files!
 //Hint 2: The header and footer are partials.
 //Hint 3: Add the CSS link in header.ejs
-
-//Step 4 - Add a dynamic year to the footer.
-//Hint: Google to find out how to get the current year using JS.
+app.use(express.static('public'));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -28,11 +29,11 @@ app.post('/submit', (req, res) => {
   //2. Send the index.ejs as a response and add the adjective and noun to the res.render
   //3. Test to make sure that the random words display in the h1 element in index.ejs
 
-  const randomAdj = adj[Math.floor(Math.random() * adj.length)];
+  const adjective = adj[Math.floor(Math.random() * adj.length)];
   const randomNoun = noun[Math.floor(Math.random() * noun.length)];
   res.render('index.ejs', {
-    adjective: randomAdj,
-    noun: randomNoun,
+    adjective,
+    randomNoun,
   });
 });
 
