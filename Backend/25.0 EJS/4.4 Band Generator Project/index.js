0a23b6a45eab479df1601a75d211a4782bf226a0
Roshan Kumar <67013997+roshanwaa@users.noreply.github.com>
2023-09-26 21:48:48 +0530

    page run

--- Backend/25.0 EJS/4.4 Band Generator Project/index.js
+++ Backend/25.0 EJS/4.4 Band Generator Project/index.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   //Step 1 - Make the get route work and render the index.ejs file.
-  res.render('index.ejs');
+  Project/TODO Application/View/layout.ejs
 });
 
 app.post('/submit', (req, res) => {
