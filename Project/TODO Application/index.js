190265915e3fb39a0fc0760addb7ef2bdbd0b1df
Roshan Kumar <67013997+roshanwaa@users.noreply.github.com>
2023-09-27 18:22:16 +0530

    TODO List Done

--- Project/TODO Application/index.js
+++ Project/TODO Application/index.js
@@ -1,9 +1,39 @@
 import express from 'express';
 const app = express();
-const port = 3000;
+const port = 3000; // You can change the port number
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+let tasks = {
+  day: [],
+  work: [],
+};
+
+// app.get('/', (req, res) => {
+//   res.render('index', { tasks });
+// });
 
 app.get('/', (req, res) => {
-  res.render('index.ejs');
+  res.render('index', { tasks, list: 'day' }); // Pass 'list' as a variable
+});
+app.post('/addTask', (req, res) => {
+  const { task, list } = req.body;
+  tasks[list].push(task);
+  res.redirect('/');
+});
+
+app.post('/completeTask/:list/:index', (req, res) => {
+  const { list, index } = req.params;
+  tasks[list][index] = `<s>${tasks[list][index]}</s>`;
+  res.redirect('/');
+});
+
+app.post('/deleteTask', (req, res) => {
+  const { list, index } = req.body;
+  tasks[list].splice(index, 1);
+  res.redirect('/');
 });
 
 app.listen(port, () => {
