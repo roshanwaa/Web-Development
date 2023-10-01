7821ee75b87e0017545847551ea9fb5703d785ce
Roshan Kumar <67013997+roshanwaa@users.noreply.github.com>
2023-10-01 23:40:55 +0530

    install module

--- Backend/28.0 APIs (Application Programming Interfaces)/5.3 Axios/index.js
+++ Backend/28.0 APIs (Application Programming Interfaces)/5.3 Axios/index.js
@@ -1,30 +1,30 @@
-import express from "express";
-import bodyParser from "body-parser";
-import axios from "axios";
+import express from 'express';
+import bodyParser from 'body-parser';
+import axios from 'axios';
 
 const app = express();
 const port = 3000;
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Step 1: Make sure that when a user visits the home page,
 //   it shows a random activity.You will need to check the format of the
 //   JSON data from response.data and edit the index.ejs file accordingly.
-app.get("/", async (req, res) => {
+app.get('/', async (req, res) => {
   try {
-    const response = await axios.get("https://bored-api.appbrewery.com/random");
+    const response = await axios.get('https://bored-api.appbrewery.com/random');
     const result = response.data;
-    res.render("index.ejs", { data: result });
+    res.render('index.ejs', { data: result });
   } catch (error) {
-    console.error("Failed to make request:", error.message);
-    res.render("index.ejs", {
+    console.error('Failed to make request:', error.message);
+    res.render('index.ejs', {
       error: error.message,
     });
   }
 });
 
-app.post("/", async (req, res) => {
+app.post('/', async (req, res) => {
   console.log(req.body);
 
   // Step 2: Play around with the drop downs and see what gets logged.
