805c6b2bd58b2de61737a3b45ceb62812a36b480
Roshan Kumar <67013997+roshanwaa@users.noreply.github.com>
2023-10-07 19:57:18 +0530

    change ejs for view more sequence

--- Backend/28.0 APIs (Application Programming Interfaces)/5.5 REST APIs/solution.js
+++ Backend/28.0 APIs (Application Programming Interfaces)/5.5 REST APIs/solution.js
@@ -22,7 +22,7 @@ app.post('/get-secret', async (req, res) => {
   const searchId = req.body.id;
   try {
     const result = await axios.get(API_URL + '/secrets/' + searchId, config);
-    res.render('index.ejs', { content: JSON.stringify(result.data) });
+    res.render('index.ejs', { content: result.data });
   } catch (error) {
     res.render('index.ejs', { content: JSON.stringify(error.response.data) });
   }
