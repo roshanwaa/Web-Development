0aa9e1b7c90446991c5950c1524ac6b10f7f3535
Roshan Kumar <67013997+roshanwaa@users.noreply.github.com>
2023-10-18 23:45:37 +0530

    done delet method

--- Backend/30.0 Build Own APIs/6.0 DIY API/index.js
+++ Backend/30.0 Build Own APIs/6.0 DIY API/index.js
@@ -88,6 +88,19 @@ app.put('/jokes/:id', (req, res) => {
 });
 //7. DELETE Specific joke
 
+app.delete('/jokes/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const searchIndex = jokes.find((val) => val.id === id);
+  if (searchIndex > -1) {
+    jokes.splice(searchIndex, 1);
+    res.sendStatus(200);
+  } else {
+    res
+      .status(404)
+      .json({ error: `Joke with id: ${id} not found. No Joke were deleted.` });
+  }
+});
+
 //8. DELETE All jokes
 
 app.listen(port, () => {
