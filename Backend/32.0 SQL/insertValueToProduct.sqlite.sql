6ea9ac741432781eb35f68526aa0d16a223ff9a1
Roshan Kumar <67013997+roshanwaa@users.noreply.github.com>
2023-10-24 19:00:09 +0530

    Insert product to the sql table

---
+++ Backend/32.0 SQL/insertValueToProduct.sqlite.sql
@@ -0,0 +1,17 @@
+-- TABLE
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE product(
+  id int NOt NULL,
+  Name STRING,
+  Price MONEY,
+  PRIMARY KEY(id)
+);
+ 
+-- INDEX
+-- INSERT product to the database
+INSERT INTO product
+VALUES(1, "Pen", 2.0)
+-- TRIGGER
+ 
+-- VIEW
+ 
