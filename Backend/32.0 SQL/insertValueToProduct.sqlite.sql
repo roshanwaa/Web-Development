c144e039489043d49ce467d3699919223780840d
Roshan Kumar <67013997+roshanwaa@users.noreply.github.com>
2023-10-24 20:59:14 +0530

    specific id of the data

--- Backend/32.0 SQL/insertValueToProduct.sqlite.sql
+++ Backend/32.0 SQL/insertValueToProduct.sqlite.sql
@@ -13,6 +13,10 @@ INSERT INTO product
 VALUES(1, "Pen", 2.0)
 VALUES(2, "Copy", 7.0)
 VALUES(3, "Book", 10.0)
+
+-- insert only id and name to the product
+INSERT INTO product(id, Name)
+VALUES(4, "Book")
 -- TRIGGER
  
 -- VIEW
@@ -20,8 +24,13 @@ VALUES(3, "Book", 10.0)
  SELECT * FROM product 
 
 -- Show the specific name and price table 
-SELECT name, price  FROM 'product' 
+SELECT Name, price  FROM 'product' 
 
 -- Show the specific id of table 
 SELECT * FROM product 
 WHERE id=1
+
+-- Update specific id of the data
+UPDATE product
+set price = 0.8
+WHERE id = 4
