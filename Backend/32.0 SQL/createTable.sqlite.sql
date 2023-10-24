df6c8802a5a246331bf64cabb44d6383ad3c6608
Roshan Kumar <67013997+roshanwaa@users.noreply.github.com>
2023-10-24 18:59:52 +0530

    Create sql Table

---
+++ Backend/32.0 SQL/createTable.sqlite.sql
@@ -0,0 +1,15 @@
+-- TABLE
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE product(
+  id int NOt NULL,
+  Name STRING,
+  Price MONEY,
+  PRIMARY KEY(id)
+);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
