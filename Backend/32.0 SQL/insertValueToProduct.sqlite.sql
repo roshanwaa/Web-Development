23ee0c80458228096e8f170a76cf17c3aab96bb5
Roshan Kumar <67013997+roshanwaa@users.noreply.github.com>
2023-10-27 19:33:12 +0530

    Understanding SQL Relationships, Foreign Keys and Inner

--- Backend/32.0 SQL/insertValueToProduct.sqlite.sql
+++ Backend/32.0 SQL/insertValueToProduct.sqlite.sql
@@ -53,3 +53,37 @@ ADD stock INT
 -- TODO:  SQL Commands: DELETE
 DELETE FROM product
 WHERE  id = 5
+
+-- TODO:  FOREIGN KEY Constraint
+/* The FOREIGN KEY constraint is used to prevent actions that would destroy links between tables.
+
+A FOREIGN KEY is a field (or collection of fields) in one table, that refers to the PRIMARY KEY in another table.
+
+The table with the foreign key is called the child table, and the table with the primary key is called the referenced or  */
+
+CREATE TABLE Orders (
+    OrderID int NOT NULL,
+    OrderNumber int NOT NULL,
+    PersonID int,
+    PRIMARY KEY (OrderID),
+    FOREIGN KEY (PersonID) REFERENCES Persons(PersonID)
+);
+
+SQL Server / Oracle / MS Access:
+
+CREATE TABLE Orders (
+    OrderID int NOT NULL PRIMARY KEY,
+    OrderNumber int NOT NULL,
+    PersonID int FOREIGN KEY REFERENCES Persons(PersonID)
+);
+
+-- To allow naming of a FOREIGN KEY constraint, and for defining a FOREIGN KEY constraint on multiple columns, use the following SQL syntax:
+
+CREATE TABLE Orders (
+    OrderID int NOT NULL,
+    OrderNumber int NOT NULL,
+    PersonID int,
+    PRIMARY KEY (OrderID),
+    CONSTRAINT FK_PersonOrder FOREIGN KEY (PersonID)
+    REFERENCES Persons(PersonID)
+);
