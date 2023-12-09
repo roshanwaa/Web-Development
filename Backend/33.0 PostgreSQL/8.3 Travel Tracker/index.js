89d7b69772a979fd6b201c9822308195803dd802
roshanwaa <kumroshan120@gmail.com>
2023-12-09 19:18:16 +0530

    255 Travel Tracker Part 1

--- Backend/33.0 PostgreSQL/8.3 Travel Tracker/index.js
+++ Backend/33.0 PostgreSQL/8.3 Travel Tracker/index.js
@@ -1,14 +1,40 @@
-import express from "express";
-import bodyParser from "body-parser";
+import express from 'express';
+import bodyParser from 'body-parser';
+import pg from 'pg';
 
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+const db = new pg.Client({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'world',
+  password: '1100',
+  port: 5432,
+});
 
-app.get("/", async (req, res) => {
+db.connect();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get('/', async (req, res) => {
   //Write your code here.
+
+  const result = await db.query('SELECT country_code FROM visited_country');
+  let countries = [];
+
+  result.rows.forEach((row) => {
+    countries.push(row.country_code);
+  });
+
+  // for (let i = 0; i < result.rows.length; i++) {
+  //   countries.push(result.rows[i].country_code);
+  //   // console.log(result.rows[i].country_code);
+  // }
+
+  res.render('index.ejs', { countries: countries, total: result.rows.length });
+  db.end();
 });
 
 app.listen(port, () => {
