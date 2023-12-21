92091f6dfcbbd11c9b35b70f539222ebf5676e58
roshanwaa <kumroshan120@gmail.com>
2023-12-21 22:39:07 +0530

    add DB Clint in index

--- Backend/33.0 PostgreSQL/8.6 Permalist Project/index.js
+++ Backend/33.0 PostgreSQL/8.6 Permalist Project/index.js
@@ -1,33 +1,43 @@
-import express from "express";
-import bodyParser from "body-parser";
+import express from 'express';
+import bodyParser from 'body-parser';
+import pg from 'pg';
 
 const app = express();
 const port = 3000;
 
+const db = new pg.Client({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'permalist',
+  password: '1100',
+  port: 5432,
+});
+db.connect();
+
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 let items = [
-  { id: 1, title: "Buy milk" },
-  { id: 2, title: "Finish homework" },
+  { id: 1, title: 'Buy milk' },
+  { id: 2, title: 'Finish homework' },
 ];
 
-app.get("/", (req, res) => {
-  res.render("index.ejs", {
-    listTitle: "Today",
+app.get('/', (req, res) => {
+  res.render('index.ejs', {
+    listTitle: 'Today',
     listItems: items,
   });
 });
 
-app.post("/add", (req, res) => {
+app.post('/add', (req, res) => {
   const item = req.body.newItem;
   items.push({ title: item });
-  res.redirect("/");
+  res.redirect('/');
 });
 
-app.post("/edit", (req, res) => {});
+app.post('/edit', (req, res) => {});
 
-app.post("/delete", (req, res) => {});
+app.post('/delete', (req, res) => {});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
