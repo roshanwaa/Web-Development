47ed587ec5f9fc1fc37eb0d1f94a2c4ff0ce4a32
roshanwaa <kumroshan120@gmail.com>
2023-12-27 01:18:30 +0530

    login and register page add

--- Backend/35.0 Authentication/Secrets - Starting Code/app.js
+++ Backend/35.0 Authentication/Secrets - Starting Code/app.js
@@ -16,6 +16,14 @@ app.get('/', (req, res) => {
   res.render('home.ejs');
 });
 
+app.get('/login', (req, res) => {
+  res.render('login.ejs');
+});
+
+app.get('/register', (req, res) => {
+  res.render('register.ejs');
+});
+
 app.listen(3000, function () {
   console.log('listening on 3000');
 });
