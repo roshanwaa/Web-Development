b3ded3371481a52b682afe1bdfab01b958566da4
roshanwaa <kumroshan120@gmail.com>
2023-12-30 00:06:30 +0530

    add 404 page and css

--- Project/News Website/Source/routes/news.js
+++ Project/News Website/Source/routes/news.js
@@ -5,7 +5,7 @@ const newsRouter = express.Router();
 newsRouter.get('', async (req, res) => {
   try {
     const fetchNews = await axios.get(
-      'https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=26f57ffe661149508088ce06b61096d8'
+      'https://newsapi.org/v2/everything?domains=wsj.com&apiKey=26f57ffe661149508088ce06b61096d8'
     );
 
     console.log(fetchNews.data);
@@ -13,12 +13,17 @@ newsRouter.get('', async (req, res) => {
     res.render('news', { articles: fetchNews.data.articles });
   } catch (error) {
     if (error.response) {
+      res.render('news', { articles: null });
+
       console.log(error.response.data);
       console.log(error.response.status);
       console.log(error.response.headers);
     } else if (error.request) {
+      res.render('news', { articles: null });
+
       console.log(error.request);
     } else {
+      res.render('news', { articles: null });
       console.log('Error: ' + error.response.status + ' ' + error.message);
     }
   }
