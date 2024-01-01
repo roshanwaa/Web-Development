b3365e6fdacfe8d6ee4982ed8f2152d8245ff2e4
roshanwaa <kumroshan120@gmail.com>
2024-01-02 00:00:13 +0530

    add search Method to page

--- Project/News Website/Source/routes/news.js
+++ Project/News Website/Source/routes/news.js
@@ -55,4 +55,28 @@ newsRouter.get('/:id', async (req, res) => {
   }
 });
 
+newsRouter.post('', async (req, res) => {
+  const search = req.body.search;
+
+  try {
+    const fetchNews = await axios.get(`${URL}${search}&apiKey=${API_KEY}`);
+    res.render('news', { articles: fetchNews.data.articles });
+  } catch (error) {
+    if (error.response) {
+      res.render('news', { articles: null });
+
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      res.render('news', { articles: null });
+
+      console.log(error.request);
+    } else {
+      res.render('news', { articles: null });
+      console.log('Error: ' + error.response.status + ' ' + error.message);
+    }
+  }
+});
+
 module.exports = newsRouter;
