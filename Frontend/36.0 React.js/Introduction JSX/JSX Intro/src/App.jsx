f557e0e262a802bea33ec8279fa6256fbb861e7c
roshanwaa <kumroshan120@gmail.com>
2024-01-07 19:42:59 +0530

    stating Into JSX in react.js

---
+++ Frontend/36.0 React.js/Introduction JSX/JSX Intro/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="">
+        <h1 className='h1Tag'>Hello World</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, obcaecati, at aperiam expedita iusto quia recusandae similique, ducimus molestiae error optio! Natus iure fugiat fugit omnis eaque pariatur animi nulla!</p>
+      </div>
+    </>
+  )
+}
+
+export default App
