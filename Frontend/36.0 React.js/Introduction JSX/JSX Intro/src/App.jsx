5cfa3b7efe0c467f437b9362d98b205a50bad404
roshanwaa <kumroshan120@gmail.com>
2024-01-07 23:51:59 +0530

    javascript Expressions in JSX & ES6 Template Literals

--- Frontend/36.0 React.js/Introduction JSX/JSX Intro/src/App.jsx
+++ Frontend/36.0 React.js/Introduction JSX/JSX Intro/src/App.jsx
@@ -1,17 +1,26 @@
 import './App.css'
 
 function App() {
+  const name = "Roshan";
 
   return (
-    <>
+    <div className>
       <div className="">
-        <h1 className='h1Tag'>Hello World</h1>
+        <h1 className='h1Tag'>Hello {name}</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, obcaecati, at aperiam expedita iusto quia recusandae similique, ducimus molestiae error optio! Natus iure fugiat fugit omnis eaque pariatur animi nulla!</p>
-        <footer>
-          <h3>💕 Roshan.waa</h3>
-        </footer>
+        <div className="container">
+          <h1 className='h1Tag'>My Favourite Food</h1>
+          <ul>
+            <li><p>Bacon</p></li>
+            <li><p>Jamon</p></li>
+            <li><p>Noodles</p></li>
+          </ul>
+        </div>
       </div>
-    </>
+      <footer>
+        <h3>💕 Roshan.waa</h3>
+      </footer>
+    </div>
   )
 }
 
@@ -19,7 +28,6 @@ export default App
 
 // In Pure JS code looks like
 
-// export default App
 // var h1 = document.createElement('h1');
 // h1.innerHTML = 'Hello World'
 // document.getElementById('root').appendChild(h1)
