1074f3184e7b30167f7e30e74e20b9760a57c807
Roshan Kumar <67013997+roshanwaa@users.noreply.github.com>
2024-01-30 00:17:10 +0530

    add locomotive scroll in file

--- Project/TwoGood.co/src/main.js
+++ Project/TwoGood.co/src/main.js
@@ -1,3 +1,8 @@
+const scroll = new LocomotiveScroll({
+  el: document.querySelector('#main'),
+  smooth: true,
+});
+
 const videoContainerAnimation = () => {
   var videoContainer = document.querySelector('#videoContainer');
   var playBtn = document.querySelector('#play');
@@ -22,3 +27,20 @@ const videoContainerAnimation = () => {
   });
 };
 videoContainerAnimation();
+
+const loadingAnimation = () => {
+  gsap.from('#page1 h1', {
+    y: 100,
+    opacity: 0,
+    delay: 0.8,
+    duration: 0.8,
+    stagger: 0.4,
+  });
+  gsap.from('#page1 #videoContainer', {
+    scale: 0.8,
+    opacity: 0,
+    delay: 1.5,
+    duration: 0.3,
+  });
+};
+loadingAnimation();
