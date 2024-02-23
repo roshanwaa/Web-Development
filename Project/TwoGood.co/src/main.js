392085766cc751a621b825a530a727577dedb409
Roshan Kumar <67013997+roshanwaa@users.noreply.github.com>
2024-02-23 10:46:48 +0530

    done page 3

--- Project/TwoGood.co/src/main.js
+++ Project/TwoGood.co/src/main.js
@@ -44,3 +44,34 @@ const loadingAnimation = () => {
   });
 };
 loadingAnimation();
+
+function cursorAnimation() {
+  document.addEventListener('mousemove', function (dets) {
+    gsap.to('#cursor', {
+      left: dets.x,
+      top: dets.y,
+    });
+  });
+  // document.querySelector("#child1").addEventListener("mouseenter",function(){
+
+  // })
+
+  // document.querySelector("#child1").addEventListener("mouseleave",function(){
+  //   gsap.to("#cursor",{
+  //     transform: 'translate(-50%,-50%) scale(0)'
+  //   })
+  // })
+  document.querySelectorAll('.child').forEach(function (elem) {
+    elem.addEventListener('mouseenter', function () {
+      gsap.to('#cursor', {
+        transform: 'translate(-50%,-50%) scale(1)',
+      });
+    });
+    elem.addEventListener('mouseleave', function () {
+      gsap.to('#cursor', {
+        transform: 'translate(-50%,-50%) scale(0)',
+      });
+    });
+  });
+}
+cursorAnimation();
