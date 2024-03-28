4849e56221db0d09b7e44e625e6d5a935660b5f9
Roshan Kumar <67013997+roshanwaa@users.noreply.github.com>
2024-03-29 00:42:14 +0530

    Migrating The Demo App to Tailwind CSS

--- Frontend/36.0 React.js/0.6 Styling React Components/src/components/AuthInputs.jsx
+++ Frontend/36.0 React.js/0.6 Styling React Components/src/components/AuthInputs.jsx
@@ -1,14 +1,6 @@
 import { useState } from 'react';
-import { styled } from 'styled-components';
 import CustomInput from './Input';
 import { Button } from './Button';
-
-const ControlContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  margin-bottom: 1.5rem;
-`;
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
@@ -31,8 +23,10 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <ControlContainer>
+    <div
+      id="auth-inputs"
+      className="w-full max-w-sm p-8 rounded shadow-md bg-gradient-to-b from-stone-700 to-slate-800 mx-auto">
+      <div className="flex flex-col gap-2 mb-6">
         <CustomInput
           label="Email"
           type="email"
@@ -48,9 +42,9 @@ export default function AuthInputs() {
             handleInputChange('password', event.target.value)
           }
         />
-      </ControlContainer>
-      <div className="actions">
-        <Button type="button" className="text-button">
+      </div>
+      <div className="flex justify-end gap-4">
+        <Button type="button" className="text-amber-400 hover:text-amber-500">
           Create a new account
         </Button>
         <Button onClick={handleLogin}>Sign In</Button>
